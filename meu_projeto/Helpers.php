9ba6d364f41b0c3bd8fca3d09b4f55c1bcc67c8a
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-08-03 17:26:27 -0300

    Commit da aula 38
    Expressões regulares

--- meu_projeto/Helpers.php
+++ meu_projeto/Helpers.php
@@ -1,5 +1,30 @@
 <?php
 
+function validarCpf(string $cpf): bool {
+    $cpf = limparNumero($cpf);
+    if (mb_strlen($cpf) != 11 || preg_match('/(\d)1{10}/', $cpf)) {
+        return false;
+    } 
+    
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+    
+        $d = ((10 * $d) % 11) % 10;
+    
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function limparNumero(string $numero): string {
+    return preg_replace('/[^0-9]/', '', $numero);
+}
+
 function dataAtual(): string {
     $diaMes = date('d');
     $diaSemana = date('w');
---
+++ meu_projeto/fundamentos/expressoes_regulares.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../sistema/configuracao.php';
+include_once '../helpers.php';
+
+$cpf = '134.201.626-26';
+
+var_dump(validarCpf($cpf));

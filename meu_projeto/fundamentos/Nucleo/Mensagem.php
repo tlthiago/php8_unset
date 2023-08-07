99a778d577ed68493553a5531393cb503fea986b
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-08-07 11:31:28 -0300

    Commit da aula 42
    Introdução ao Encadeamento de Métodos

--- meu_projeto/fundamentos/Nucleo/Mensagem.php
+++ meu_projeto/fundamentos/Nucleo/Mensagem.php
@@ -8,12 +8,36 @@ class Mensagem {
     private $texto;
     private $css;
 
+    public function sucesso(string $mensagem): Mensagem {
+        $this->css = 'alert alert-success';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function erro(string $mensagem): Mensagem {
+        $this->css = 'alert alert-danger';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function alerta(string $mensagem): Mensagem {
+        $this->css = 'alert alert-warning';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
+    public function informa(string $mensagem): Mensagem {
+        $this->css = 'alert alert-primary';
+        $this->texto = $this->filtrar($mensagem);
+        return $this;
+    }
+
     /**
      * Método responsável pela renderização
      * @return string
      */
     public function renderizar(): string {
-        return $this->texto = $this->filtrar('<h1>Mensagem de teste');
+        return "<div class='{$this->css}'>{$this->texto}</div>";
     }
 
     private function filtrar(string $mensagem): string {
--- meu_projeto/fundamentos/introducao_as_classes.php
+++ meu_projeto/fundamentos/introducao_as_classes.php
@@ -1,9 +1,14 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
 <?php
 
 include './Nucleo/Mensagem.php';
 
 $msg = new Mensagem();
-echo $msg->renderizar();
+echo $msg->sucesso('Mensagem de sucesso')->renderizar();
+echo $msg->erro('Mensagem de erro')->renderizar();
+echo $msg->alerta('Mensagem de alerta')->renderizar();
+echo $msg->informa('Mensagem de informações')->renderizar();
 echo '<hr>';
 
 var_dump($msg);

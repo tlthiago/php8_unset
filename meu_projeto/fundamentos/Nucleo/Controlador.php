6164e0a989b489577720adf4bf314f07adb3bd51
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-08-07 12:21:56 -0300

    Commit da aula 46
    Introdução ao Método Mágico Construct

---
+++ meu_projeto/fundamentos/Nucleo/Controlador.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace fundamentos\Nucleo;
+
+class Controlador {
+    public function __construct(string $tema = null) {
+        echo $tema;
+    }
+}
--- meu_projeto/index.php
+++ meu_projeto/index.php
@@ -1,93 +1,12 @@
 <?php
 
-// Arquivo index responsável pela inicialização do sistema
-// declare(stric_types = 1);
-require_once 'sistema/configuracao.php';
-include_once 'helpers.php';
+require_once './fundamentos/configuracao.php';
+include_once './fundamentos/Nucleo/Helpers.php';
+include './fundamentos/Nucleo/Mensagem.php';
+include './fundamentos/Nucleo/Controlador.php';
 
-// $texto = '<h1>Texto</h1> <p>para</p> resumir';
-// $texto = strip_tags($texto);
+use fundamentos\Nucleo\Controlador;
 
-// echo $total = mb_strlen(trim($texto));
-// echo '<hr>';
-
-// echo $resumo = mb_substr($texto, 1, 3);
-// echo '<hr>';
-
-// echo $ocorrencia = mb_strrpos($texto, 'o');
-
-// echo resumirTexto($texto, 15);
-
-// $valor = 5;
-
-// if ($valor) {
-//     echo $valor;
-// } else {
-//     echo 0;
-// }
-
-// echo ($valor ? $valor : 0);
-// echo ($valor ?  : 0);
-
-// echo formatarValor();
-// echo formatarNumero('100');
-
-// $data = date('d/m/Y H:i:s');
-// echo $data;
-
-// echo contarTempo('2023-07-22 12:26:14');
-
-// if (validarEmail('teste@martminas.com.br')) {
-//     echo 'Endereço de e-mail válido!';
-// } else {
-//     echo 'Endereço de e-mail inválido!';
-// }
-
-// var_dump(validarUrl('https://teste.com'));
-
-// $url = 'http://unset.com';
-// var_dump(validarUrl($url));
-// echo '<hr>';
-// var_dump(validarUrlComFiltro($url));
-
-// var_dump(localhost());
-// echo '<hr>';
-// echo url('admin');
-// echo '<hr>';
-// var_dump($_SERVER);
-
-// $meses = array();
-// $meses = [
-//     'Janeiro', 
-//     'Fevereiro', 
-//     'Março'
-// ];
-
-// foreach ($meses as $chave => $valor) {
-//     echo $chave . ' - ' . $valor . '<br>';
-// }
-// echo '<hr>';
-
-// echo $meses[2];
-
-// echo '<hr>';
-// var_dump($meses);
-// echo '<hr>';
-
-// echo $_SERVER['SCRIPT_FILENAME'];
-// echo '<hr>';
-
-// var_dump($_SERVER);
-
-// echo saudacao().' Hoje é '.dataAtual();
-
-// echo slug("Adão \"Negro\" - '2022'").'<hr>';
-// echo slug("Avatar 2: O Caminho da Água").'<hr>';
-// echo slug("Não! Não Olhe!").'<hr>';
-// echo slug("Sonic 2 - O Filme").'<hr>';
-// echo slug("NOVA SÉRIE NO DISNEY+!").'<hr>';
-// echo slug("100 Melhores filmes").'<hr>';
-// echo slug("teste!@###$%6¨%%¨,*.:/?\|,").'<hr>';
-
-// echo saudacao();
-
+$controlador = new Controlador();
+echo '<hr>';
+var_dump($controlador);
